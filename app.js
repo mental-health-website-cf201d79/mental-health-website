45a9951f87a2267726375de86231722d556ed0ad
Chloe Nott <chloe@nott.email>
2021-10-05 15:30:09 -0700

    Added function to add question to page. CN/BW

--- app.js
+++ app.js
@@ -12,15 +12,34 @@
 
 
 // Constructor function for question objects.
-function Question(question, questionCategory, responseOptions, userResponse) {
+function Question(question, questionCategory, responseOptions) {
     this.question = question;                   // string
     this.questionCategory = questionCategory;   // string
     this.responseOptions = responseOptions;     // array of strings or numbers?
-    this.userResponse = userResponse;           // index in responseOptions
+    this.userResponse;           // index in responseOptions
 }
 
+// Temporary question object.
+let question1 = new Question("What is your favorite color?", "Depression", [0,1,2,3])
+askQuestion(question1);
+
 // Function to display one question on webpage.
-function askQuestion() {}
+function askQuestion(questionObject) {
+    // Display question on page.
+    let mainEl = document.getElementsByTagName("main")[0];
+    let questionEl = document.createElement("p");
+    questionEl.innerText = questionObject.question;
+    mainEl.appendChild(questionEl);
+
+    // Display answer choices on page.
+    let answersEl = document.createElement("ul");
+    for (i = 0; i < questionObject.responseOptions.length; i++) {
+        let answerEl = document.createElement("li");
+        answerEl.innerText = questionObject.responseOptions[i];
+        answersEl.appendChild(answerEl);
+    }
+    mainEl.appendChild(answersEl);
+}
 
 // Function to keep track of which questions have been asked / which questions to ask next.
 function askQuestions() {}
