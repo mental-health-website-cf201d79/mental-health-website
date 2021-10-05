b60c64e841a293dfadfeeed0bfc046e1b8f8c289
Chloe Nott <chloe@nott.email>
2021-10-05 15:01:46 -0700

    Constructor function made. Chloe driver, brandon navigator

--- app.js
+++ app.js
@@ -7,6 +7,26 @@
 // Stats page
 
 
+
 // Resource page
 
-//function 
+
+// Constructor function for question objects.
+function Question(question, questionCategory, responseOptions, userResponse) {
+    this.question = question;                   // string
+    this.questionCategory = questionCategory;   // string
+    this.responseOptions = responseOptions;     // array of strings or numbers?
+    this.userResponse = userResponse;           // index in responseOptions
+}
+
+// Function to display one question on webpage.
+function askQuestion() {}
+
+// Function to keep track of which questions have been asked / which questions to ask next.
+function askQuestions() {}
+
+// Calculates results of user responses into format results chart can use.
+function getResults() {}
+
+// Displays results chart.
+function showResults() {}
