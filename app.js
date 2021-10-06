d4790573e47e0bf8fbfaf563b794beedd01aa5b1
Chloe Nott <chloe@nott.email>
2021-10-05 18:14:03 -0700

    Added more functions for asking questions and keeping score. Todo is to get results only after game ends, and remove event handler at end. CN/BW

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
-// Home page
-
-
-// Questionnaire
-
-
-// Stats page
-
-
-
-// Resource page
-
-
 // Constructor function for question objects.
-function Question(question, questionCategory, responseOptions) {
+function Question(question, questionCategory, responseOptions, responseOptionScores) {
     this.question = question;                   // string
     this.questionCategory = questionCategory;   // string
-    this.responseOptions = responseOptions;     // array of strings or numbers?
-    this.userResponse;           // index in responseOptions
+    this.responseOptions = responseOptions;     // array of strings
+    this.responseOptionScores = responseOptionScores;   // array of numbers
+    this.userResponse;                          // index in responseOptions
 }
-
-// Temporary question object.
-let question1 = new Question("What is your favorite color?", "Depression", [0,1,2,3])
-let questionObjects = [];
-questionObjects.push(question1);
-askQuestion(question1);
 
 // Function to display one question on webpage.
 function askQuestion(questionObject) {
-    // Add question title to page.
+    // Clear page.
     let mainEl = document.getElementsByTagName("main")[0];
-    let questionTitle = document.createElement("p");
+    mainEl.innerText = "";
+
+    // Add question title to page.
+    let questionTitle = document.createElement("h1");
     for (let i = 0; i < questionObjects.length; i++) {
         if (questionObjects[i].question == questionObject.question) {
             questionTitle.innerText = `Question ${i+1} of ${questionObjects.length}`;
@@ -51,13 +36,88 @@ function askQuestion(questionObject) {
         answersEl.appendChild(answerEl);
     }
     mainEl.appendChild(answersEl);
+
+    document.getElementsByTagName("ul")[0].addEventListener("click", handleResponse);   // Wait for user to click their response choice.
 }
 
-// Function to keep track of which questions have been asked / which questions to ask next.
-function askQuestions() {}
+// Function to handle user response (click).
+// Records which response option the user selected.
+function handleResponse(event) {
+    let elementClicked = event.target; // returns the list element "<li>3</li>" that was clicked on
+    
+    // Record user selection.
+    let questionObject;
 
+    // Find the questionObject that corresponds to the question the user responded to.
+    for (i = 0; i < questionObjects.length; i++) {
+        if (document.getElementsByTagName("p")[0].innerText == questionObjects[i].question) {
+            questionObjects[i].userResponse = Array.from(elementClicked.parentNode.children).indexOf(elementClicked);
+            break;
+        }
+    }
+    // Ask next question.
+    askNextQuestion();
+}
+
+function askNextQuestion() {
+    // Ask all the questions in questionObjects. Do not ask repeat questions.
+    for (let i = 0; i < questionObjects.length; i++) {
+        // if questoinObjects[i] is in questionsAlreadyAsked, go to next questionObject
+        if (questionsAlreadyAsked.includes(questionObjects[i])) {
+            // do nothing, go to next question.
+        } else {
+            // else ask the question and push it into the questions already asked array.
+            questionsAlreadyAsked.push(questionObjects[i]); // Keep track of which questions the user has been asked.
+            askQuestion(questionObjects[i]);
+            break;
+        }
+    }
+    getResults();
+}
+t 
 // Calculates results of user responses into format results chart can use.
-function getResults() {}
+function getResults() {
+
+    // For loop to loop through questionObjects.
+    // After this loop completes should have array that looks like this: ["Depression", "GAD", "SAD", "ADHD", "PTSD", "GD"];
+    let questionCategories = [];
+    let sumOfEachCategory = [];
+    for (let i = 0; i < questionObjects.length; i++) {
+        // Push new questionCategories into an array to keep track of which questionCategories exist
+        if (questionCategories.includes(questionObjects[i].questionCategory)) {
+            // Question category is already in the questionCategories list, so do nothing.
+        } else {
+            questionCategories.push(questionObjects[i].questionCategory)
+            sumOfEachCategory.push(0);
+        }
+    }
+
+    // New array to keep track of total values for each category: sumOfEachCategory = [sumOfDepressionScores, sumofGADScores, ...]
+    // For loop to assemble the results for each index in the question categories array.
+    for (let j = 0; j < questionObjects.length; j++) {
+        for (let i = 0; i < questionCategories.length; i++) {
+            if (questionCategories[i] == questionObjects[j].questionCategory) {
+                sumOfEachCategory[i] = sumOfEachCategory[i] + questionObjects[j].responseOptionScores[Number(questionObjects[j].userResponse)];
+            }
+        }
+    }
+
+    console.log(questionCategories);
+    console.log(sumOfEachCategory);
+
+    // Goal is to get array of questionCategories.
+    // 2nd goal is to get array of scores for each question category (sumOfEachCategory).
+}
 
 // Displays results chart.
 function showResults() {}
+
+// Temporary question object.
+let question1 = new Question("What is your favorite color?", "Depression", ["Not likely","Sometimes","Never","Occasionally"], [0, 5, 10, 15])
+let question2 = new Question("What is your favorite color2?", "Depression2", [5,6,7,8], [0, 6, 11, 16])
+let questionObjects = [question1, question2];
+
+
+// Keep track of which questions have already been asked.
+let questionsAlreadyAsked = [];
+askNextQuestion();
