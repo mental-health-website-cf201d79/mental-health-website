57b499b74d10eaf5a00b2f92e05463422e8d1c51
ibraheem ibraheem <iibraheem2001@gmail.com>
2021-10-07 11:58:28 -0700

    created charts and worked on formatting/styling the quiz

--- stats.js
+++ stats.js
@@ -1,34 +1,16 @@
-new Chart(document.getElementById("asd-prevalence").getContext("2d"), {
-    type: 'doughnut',
+new Chart(document.getElementById("prevalence-chart").getContext("2d"), {
+    type: 'bar',
     data: {
-        labels: ['Other','Autism'],
+        labels: ['Estimated','Overall','Female','Male','18-25','26-49','50+','Hispanic','White','Black or African American','Asian','NH/OPI','AI/AN','2 or More'],
         datasets: [{
-            label: 'Autism Prevalence',
-            labels: ['Other','Autism'],
-            data: [54, 1],
+            label: 'Mental Health Prevalence',
+            data: [26,20.6,24.5,16.3,29.4,25.0,14.0,18.0,22.2,17.3,14.4,16.6,18.7,31.7],
             backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)'],
+                'rgb(255, 99, 132)'],
             hoverOffset: 4
         }]
     },
     options: {
         responsive: true
-    }
-});
-
-new Chart(document.getElementById("asd-prevalence2").getContext("2d"), {
-    type: 'doughnut',
-    data: {
-        labels: ['Other','Autism'],
-        datasets: [{
-            label: 'Autism Prevalence',
-            labels: ['Other','Autism'],
-            data: [54, 1],
-            backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)'],
-            hoverOffset: 4
-        }]
     }
 });
