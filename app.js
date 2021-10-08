ada860d312716fa7c1c5189af79950bcbb4dab8c
Chloe Nott <chloe@nott.email>
2021-10-07 17:11:23 -0700

    Adjusted chart sizes. Added descriptions onto results page, see app.js

--- app.js
+++ app.js
@@ -114,10 +114,10 @@ function getResults() {
     // 2nd goal is to get array of scores for each question category (sumOfEachCategory).
 }
 
-// Displays results chart.
+// Displays results chart and mental health descriptions.
 function showResults(x, y) {
     
-    console.log("hi")        
+    // Adds results chart to webpage.
     let mainEl = document.getElementsByTagName("main")[0]
     mainEl.innerText = ""
     let canvasEl = document.createElement("canvas")
@@ -144,6 +144,59 @@ function showResults(x, y) {
             }
         }
     });
+
+    let descriptionObjects = {
+        conditions: ['Depression','Generalized Anxiety','Social Anxiety','ADHD','Gender Dysphoria','PTSD'],
+        links: [
+            'https://www.nimh.nih.gov/health/topics/depression',
+            'https://www.nimh.nih.gov/health/publications/generalized-anxiety-disorder-gad',
+            'https://www.nimh.nih.gov/health/statistics/social-anxiety-disorder',
+            'https://www.nimh.nih.gov/health/topics/attention-deficit-hyperactivity-disorder-adhd',
+            'https://en.wikipedia.org/wiki/Gender_dysphoria',
+            'https://www.nimh.nih.gov/health/topics/post-traumatic-stress-disorder-ptsd'
+        ],
+        descriptions: [
+            `Depression (major depressive disorder or clinical depression) is a common but serious mood disorder. It causes severe symptoms that affect how you feel, think, and handle daily activities, such as sleeping, eating, or working.`,
+            `Occasional anxiety is a normal part of life. You might worry about things like health, money, or family problems. But people with generalized anxiety disorder (GAD) feel extremely worried or feel nervous about these and other things—even when there is little or no reason to worry about them. People with GAD find it difficult to control their anxiety and stay focused on daily tasks.`,
+            `Social anxiety disorder (formerly social phobia) is characterized by persistent fear of one or more social or performance situations in which the person is exposed to unfamiliar people or to possible scrutiny by others. The individual fears that he or she will act in a way (or show anxiety symptoms) that will be embarrassing and humiliating.`,
+            `Attention-deficit/hyperactivity disorder (ADHD) is marked by an ongoing pattern of inattention and/or hyperactivity-impulsivity that interferes with functioning or development. [...] Some people with ADHD mainly have symptoms of inattention. Others mostly have symptoms of hyperactivity-impulsivity. Some people have both types of symptoms.`,
+            `Gender dysphoria (GD) is the distress a person feels due to a mismatch between their gender identity—their personal sense of their own gender—and their sex assigned at birth.`,
+            `Post-traumatic stress disorder (PTSD) is a disorder that develops in some people who have experienced a shocking, scary, or dangerous event. It is natural to feel afraid during and after a traumatic situation. Fear triggers many split-second changes in the body to help defend against danger or to avoid it.`
+        ],
+        source: [
+            '—National Institute of Mental Health',
+            '—National Institute of Mental Health',
+            '—National Institute of Mental Health',
+            '—National Institute of Mental Health',
+            '—Wikipedia',
+            '—National Institute of Mental Health'
+        ]
+    }
+
+    // Add condition descriptions contained in discriptionObjects to page.
+    let figureContainer = document.createElement("div");
+    figureContainer.setAttribute("id", "figure-container");
+
+    for (i = 0; i < descriptionObjects.conditions.length; i++) {
+        let definitionFig = document.createElement("figure");
+        figureContainer.appendChild(definitionFig);
+    
+        let link = document.createElement("figcaption");
+        link.innerText = descriptionObjects.conditions[i];
+        definitionFig.appendChild(link);
+    
+        let quote = document.createElement("blockquote");
+        quote.innerText = descriptionObjects.descriptions[i];
+        definitionFig.appendChild(quote);
+    
+        let source = document.createElement("a");
+        source.innerText = descriptionObjects.source[i];
+        source.href = descriptionObjects.links[i];
+        definitionFig.appendChild(source);
+    }
+
+    mainEl.appendChild(figureContainer);
+
 }
 
 
@@ -154,6 +207,11 @@ QuestionCategory = ['Depression','Depression','Depression','Generalized Anxiety'
 Questions = ['Little interest or pleasure in doing things','Feeling down, depressed, or hopeless','Thoughts that you would be better off dead, or thoughts of hurting yourself in some way?','Feeling nervous, anxious, or on edge','Not being able to stop or control worrying','I find it difficult to mix comfortably with the people I work with.','I am at ease meeting people at parties, etc.','I have difficulty talking with other people.','I worry about expressing myself in case I appear awkward.','I find myself worrying that I won\'t know what to say in social situations.','How often do you have trouble wrapping up the final details of a project, once the challenging parts have been done?','How often do you have difficulty getting things in order when you have to do a task that requires organization?','How often do you have problems remembering appointments or obligations?','When you have a task that requires a lot of thought, how often do you avoid or delay getting started?','How often do you fidget or squirm with your hands or feet when you have to sit down for a long time?','How often do you feel overly active and compelled to do things, like you were driven by a motor?','Every time someone treats me like my assigned sex I feel hurt','It feels good to live as my affirmed gender','A life in my affirmed gender is more attractive for me than a life in my assigned sex','I wish I had been born as my affirmed gender','I feel unhappy when someone misgenders me','Memories of trauma come to mind at unwanted moments','At random times, I feel a burst of anger and/or guilt about past trauma','I feel a physical effect when remembering a past trauma, effects can range from racing pulse, excessive sweating, chills or shaking?']
 ResponseOptions = [['Not at all','Several days','More than half the days','Nearly every day'],['Not at all','Several days','More than half the days','Nearly every day'],['Not at all','Several days','More than half the days','Nearly every day'],['Not at all','Several days','More than half the days','Nearly every day'],['Not at all','Several days','More than half the days','Nearly every day'],['Not at all','Slightly','Moderately','Very','Extremely'],['Not at all','Slightly','Moderately','Very','Extremely'],['Not at all','Slightly','Moderately','Very','Extremely'],['Not at all','Slightly','Moderately','Very','Extremely'],['Not at all','Slightly','Moderately','Very','Extremely'],['Never','Rarely','Sometimes','Often','Very Often'],['Never','Rarely','Sometimes','Often','Very Often'],['Never','Rarely','Sometimes','Often','Very Often'],['Never','Rarely','Sometimes','Often','Very Often'],['Never','Rarely','Sometimes','Often','Very Often'],['Never','Rarely','Sometimes','Often','Very Often'],['Disagree completely','Disagree','Neither agree nor disagree','Agree','Agree completely'],['Disagree completely','Disagree','Neither agree nor disagree','Agree','Agree completely'],['Disagree completely','Disagree','Neither agree nor disagree','Agree','Agree completely'],['Disagree completely','Disagree','Neither agree nor disagree','Agree','Agree completely'],['Disagree completely','Disagree','Neither agree nor disagree','Agree','Agree completely'],['Disagree completely','Disagree','Neither agree nor disagree','Agree','Agree completely'],['Disagree completely','Disagree','Neither agree nor disagree','Agree','Agree completely'],['Disagree completely','Disagree','Neither agree nor disagree','Agree','Agree completely']]
 ScoringKey = [[0,11.11,22.22,33.33],[0,11.11,22.22,33.33],[0,11.11,22.22,33.33],[0,16.67,33.33,50],[0,16.67,33.33,50],[0,5,10,15,20],[20,15,10,5,0],[0,5,10,15,20],[0,5,10,15,20],[0,5,10,15,20],[0,0,16.67,16.67,16.67],[0,0,16.67,16.67,16.67],[0,0,16.67,16.67,16.67],[0,0,0,16.67,16.67],[0,0,0,16.67,16.67],[0,0,0,16.67,16.67],[0,0,0,10,20],[0,0,0,10,20],[0,0,0,10,20],[0,0,0,10,20],[0,0,0,10,20],[0,0,0,16.67,33.33],[0,0,0,16.67,33.33],[0,0,0,16.67,33.33]]
+
+QuestionCategory = ['hi']
+Questions = ['hi']
+ResponseOptions = [['hi']]
+scoringkey = [[1]]
 
 
 let questionObjects = [];
